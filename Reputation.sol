7306e118c3befb2b2e8c4cb617da27f9522c5be1
eberhartp <pacome.eberhart@gmail.com>
2019-11-08 15:01:53 +0100

    Passage à une struct pour User, ajout liste users

--- Reputation.sol
+++ Reputation.sol
@@ -3,30 +3,44 @@ pragma solidity ^0.5.11;
 import "./Administrable.sol";
 
 contract Reputation is Administrable {
-    mapping(address => uint256) private _reputations;
-    mapping(address => string) private _names;
-    mapping(address => bool) private _users;
-    mapping(address => bool) private _bannedUsers;
+    struct User {
+        bool registered;
+        string name;
+        uint256 reputation;
+        bool banned;
+    }
+
+    mapping(address => User) private _users;
+    address[] private _userAddresses;
 
     function registration(string memory name) public {
-        require(!_users[msg.sender], "User already registered");
-        require(!_bannedUsers[msg.sender], "User banned");
-        _users[msg.sender] = true;
-        _reputations[msg.sender] = 1;
-        _names[msg.sender] = name;
+        require(!_users[msg.sender].registered, "User already registered");
+        require(!_users[msg.sender].banned, "User banned");
+        _users[msg.sender].reputation = 1;
+        _users[msg.sender].name = name;
+        _users[msg.sender].registered = true;
+        _userAddresses.push(msg.sender);
+    }
+
+    function getUsersCount() public view returns(uint256) {
+        return _userAddresses.length;
+    }
+
+    function getUserDetails(address user) public view returns(string memory, uint256, bool) {
+        return (_users[user].name, _users[user].reputation, _users[user].banned);
     }
 
     function banUser(address user) public onlyAdmin {
-        require(_users[user], "Unknown user");
-        require(!_bannedUsers[user], "User already banned");
-        _bannedUsers[user] = true;
-        _reputations[user] = 0;
+        require(_users[user].registered, "Unknown user");
+        require(!_users[user].banned, "User already banned");
+        _users[user].banned = true;
+        _users[user].reputation = 0;
     }
 
     function unbanUser(address user) public onlyAdmin {
-        require(_users[user], "Unknown user");
-        require(_bannedUsers[user], "User not banned");
-        _bannedUsers[user] = false;
-        _reputations[user] = 1;
+        require(_users[user].registered, "Unknown user");
+        require(_users[user].banned, "User not banned");
+        _users[user].banned = false;
+        _users[user].reputation = 1;
     }
 }
